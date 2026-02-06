7c2d2580b4eba898e07a1fc40087c64c8caca1ba
Clément Mistral <55872681+clement-mistral@users.noreply.github.com>
2026-02-06 14:36:07 +0100

    feat ✨ (auth): Added Auth components and page
    Signed-off-by: Clément Mistral <55872681+clement-mistral@users.noreply.github.com>

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,12 +1,30 @@
-import { Head } from "@inertiajs/react";
+import { Head, router } from "@inertiajs/react";
+import Button from "@/components/ui/button";
 
-export default function Home() {
+export default function Home({
+	user,
+}: {
+	user: {
+		fullName: string;
+		email: string;
+	};
+}) {
+	const handleLogout = () => {
+		router.delete("/auth");
+	};
+
 	return (
 		<>
 			<Head title="Homepage" />
 
 			<div className="pt-4 h-full flex flex-col">
 				<h1 className="text-primary">Hello world</h1>
+				{user.fullName}
+				<div className="w-48">
+					<Button type="button" onClick={handleLogout}>
+						Se déconnecter
+					</Button>
+				</div>
 			</div>
 		</>
 	);
